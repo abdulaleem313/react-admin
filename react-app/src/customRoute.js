33467a29d8ef388203433ce5e6e8eee750773ff6
abdulaleem313 <abdulaleem313@yahoo.com>
2018-11-19 15:46:33 +0500

    create category page

--- react-app/src/customRoute.js
+++ react-app/src/customRoute.js
@@ -5,9 +5,13 @@ import UserListing from './userListing';
 import CreateUser from './users/create-user'; 
 import UpdateUser from './users/update-user'; 
 
+import CreateCategory from './categories/create-category'; 
+
 export default [
     <Route exact path="/dashboard" component={Dashboard} />, 
     <Route exact path="/users" component={UserListing} />, 
     <Route exact path="/create-user" component={CreateUser} />, 
-    <Route exact path="/update-user/:id" component={UpdateUser} />, 
+    <Route exact path="/update-user/:id" component={UpdateUser} />,
+     
+    <Route exact path="/category/create" component={CreateCategory} />, 
 ];
